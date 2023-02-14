04db43cc8b1947603de89aa2681193e4a90acab6
55973 <55973@etu.he2b.be>
2023-02-14 11:08:23 +0100

    fini

--- src/components/horaire/horaire.js
+++ src/components/horaire/horaire.js
@@ -33,34 +33,36 @@ export const Horaire = () => {
         <div>
 
 
-            <div className="flex justify-center flex-col max-sm:pt-16 sm:h-screen bg-blue-b w-full">
+            <div className="flex justify-center align-middle items-center max-md:pb-20 max-sm:pt-16 md:h-screen bg-blue-b w-full">
                 <div>
-                    <div className='md:flex justify-center'>
+                    <div className='flex justify-center max-md:flex-col max-md:items-center'>
 
                         <Personne buttonDaily={buttonDaily} buttonWeek={buttonWeek} buttonMonthly={buttonMonthly} />
 
 
-                        <div id='haut' className='flex md:w-6/12 max-md:w-[80%] grap-5 flex-wrap' >
+                        
 
-                            {data.map((donnees) => (
+                            <div id='haut' className='flex md:w-6/12 max-md:w-[80%] grap-5 flex-wrap' >
 
-                                <div id="par" className='sm:mx-3 mb-5 rounded-xl w-48 max-sm:columns-1 max-sm:mt-5 max-md:w-full  mx-auto'>
-                                    <div className='bg-blue-c hover:bg-blue-900   text-white px-7  py-3 rounded-xl'>
-                                        <ul key={donnees.title}>
-                                            <li className='flex align-middle max-sm:font-semibold justify-between my-2'><div>{donnees.title}</div>
-                                                <div className='my-auto hover:cursor-pointer'>{svg1}</div></li>
-                                            <ul className="max-sm:flex max-sm:justify-between max-sm:align-baseline">
-                                                <li className='text-4xl font-light mt-3 mb-3'>{donnees.timeframes[timeframes].current}hrs</li>
-                                                <li className=' text-gray-400 text-xs flex max-sm:items-center'>Last Week - {donnees.timeframes[timeframes].previous}hrs</li>
+                                {data.map((donnees) => (
+
+                                    <div id="par" className='sm:mx-3 mb-5 rounded-xl w-48 max-sm:columns-1 max-sm:mt-5 max-md:w-full  mx-auto'>
+                                        <div className='bg-blue-c hover:bg-blue-900   text-white px-7  py-3 rounded-xl'>
+                                            <ul key={donnees.title}>
+                                                <li className='flex align-middle max-sm:font-semibold justify-between my-2'><div>{donnees.title}</div>
+                                                    <div className='my-auto hover:cursor-pointer'>{svg1}</div></li>
+                                                <ul className="max-sm:flex max-sm:justify-between max-sm:align-baseline">
+                                                    <li className='text-4xl font-light mt-3 mb-3'>{donnees.timeframes[timeframes].current}hrs</li>
+                                                    <li className=' text-gray-400 text-xs flex max-sm:items-center'>Last Week - {donnees.timeframes[timeframes].previous}hrs</li>
+                                                </ul>
                                             </ul>
-                                        </ul>
+                                        </div>
                                     </div>
-                                </div>
 
-                            ))}
+                                ))}
 
-                        </div>
-
+                            </div>
+                     
 
 
                     </div>
